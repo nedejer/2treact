cec673335bc143bcf5c980c32f83d0f24e070753
nedejer <noera.noelani@gmail.com>
2025-12-11 13:04:19 -1000

    burgermenu

---
+++ index.js
@@ -0,0 +1,10 @@
+<script>
+  function openMenu() {
+    document.getElementById("mobileMenu").classList.add("open");
+  }
+
+  function closeMenu() {
+    document.getElementById("mobileMenu").classList.remove("open");
+  }
+</script>
+
